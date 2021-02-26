41274eae2da85b097591d539e85a458f63d8bc84
cassymarie <cassy2205@gmail.com>
2021-02-26 12:34:21 -0700

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'https://cassymarie.github.io/let-it-go_fe/'
 
-    resource 'https://cassymarie.github.io/let-it-go_fe/',
+    resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
